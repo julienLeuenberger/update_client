4a48c8b6f0c63868b1e4cc0cad00acd89a19b168
Julien Leuenberger <julien.leuenberger@heig-vd.ch>
2021-12-05 20:32:54 +0100

    Update MbedApplication.cpp

--- MbedApplication.cpp
+++ MbedApplication.cpp
@@ -252,10 +252,19 @@ int32_t MbedApplication::readApplicationHeader() {
     switch (m_applicationHeader.headerVersion) {
       case HEADER_VERSION_V2: {
         // TODO : check magic, if successful read the entire header and call parseInternalHeaderV2
-        result = UC_ERR_NONE;
-        uint8_t read_buffer[HEADER_SIZE_V2] = {0};
-        m_flashUpdater.read(read_buffer, m_applicationHeaderAddress, HEADER_SIZE_V2);
-        result = parseInternalHeaderV2(read_buffer);
+        if (m_applicationHeader.magic == 0x5a51b3d4)
+        {
+          tr_debug("Magic number is equal to 0x5a51b3d4");
+          result = UC_ERR_NONE;
+          uint8_t read_buffer[HEADER_SIZE_V2] = {0};
+          m_flashUpdater.read(read_buffer, m_applicationHeaderAddress, HEADER_SIZE_V2);
+          result = parseInternalHeaderV2(read_buffer);
+        }
+        else
+        {
+          tr_debug("Magic number is not equal to 0x5a51b3d4");
+        }
+        
       }   
       break;
 
